1aaab2052c1abe19d26af3d2ca5cecbb548accd9
SzymonSt <szymonst2808@gmail.com>
2023-09-12 20:48:55 +0200

    k8s odgios tests with go, dotnet and python

--- dummy-score-service/go.mod
+++ dummy-score-service/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.opentelemetry.io/otel v1.18.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
@@ -36,6 +37,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	go.mongodb.org/mongo-driver v1.12.1
+	go.opentelemetry.io/otel/trace v1.18.0
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- dummy-score-service/pkg/grpc_server/grpc_server.go
+++ dummy-score-service/pkg/grpc_server/grpc_server.go
@@ -2,10 +2,13 @@ package grpc_server
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	scoreservicepb "github.com/SzymonSt/autoinstrumentation-playground/dummy-proto/scoreservice-go/v1"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type ScoreServiceServer struct {
@@ -24,11 +27,16 @@ func NewScoreServiceServer(dbClient *mongo.Client) *ScoreServiceServer {
 
 func (ss *ScoreServiceServer) SubmitScore(ctx context.Context, req *scoreservicepb.ScoreRequest) (*scoreservicepb.ScoreResponse, error) {
 	var err error
+	span := trace.SpanFromContext(ctx)
 	record := &scoreservicepb.ScoreRecord{
 		UserProfileName: req.UserProfile,
 		Score:           req.Score,
 		Time:            time.Now().Unix(),
 	}
+	fmt.Printf("trace id: %s", span.SpanContext().TraceID())
+	span.SetAttributes(
+		attribute.String("user_profile", record.UserProfileName),
+	)
 	ss.dbClient.Database(ss.dbName).Collection("scores").InsertOne(ctx, record)
 	return &scoreservicepb.ScoreResponse{
 		ScoreRecord: record,
